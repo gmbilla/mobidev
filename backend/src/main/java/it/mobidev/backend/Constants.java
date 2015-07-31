1a35becff72fbf6a7a2bc8541d92a7c9a3fbd949
Gian Marco Sibilla <gm.sibilla@gmail.com>
2015-07-31 11:51:52 +0200

    All backend code moved to new directory

--- backend/src/main/java/it/mobidev/backend/Constants.java
+++ backend/src/main/java/it/mobidev/backend/Constants.java
@@ -5,7 +5,7 @@ package it.mobidev.backend;
  */
 public class Constants {
 
-    public static final String WEB_CLIENT_ID = "replace this with your web client ID";
+    public static final String WEB_CLIENT_ID = "750859415890-gtb3anfc7255kv9t2lthne981hinkjcr.apps.googleusercontent.com";
     public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
     public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
     public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
